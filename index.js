66879a42df204c85608bc50e4b059773c7217a57
yair-shtern <101128645+yair-shtern@users.noreply.github.com>
2024-08-11 23:46:05 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -8,14 +8,6 @@ const port = 3000;
 const API_URL = "http://www.omdbapi.com/";
 const yourAPIKey = "75d3503a";
 
-// const db = new pg.Client({
-//   user: "postgres",
-//   host: "localhost",
-//   database: "movies project",
-//   password: "MyDB",
-//   port: 5432,
-// });
-
 const db = new pg.Client({
   user: "movies_uq5t_user",
   host: "dpg-cqsbes0gph6c73a96bug-a",
@@ -23,13 +15,9 @@ const db = new pg.Client({
   password: "zRav3FUUAJ6jRJUsZ7yBB7uHXcrujd8J",
   port: 5432,
 });
-// const { Pool } = pg;
-
-// const db = new Pool({
-//   connectionString: "postgres://postgres:MyDB@localhost:5432/movies project",
-// });
 
 db.connect();
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -77,35 +65,78 @@ app.post("/rate", async (req, res) => {
   const movieName = req.body.movieName;
 
   try {
+    // Step 1: Search for movies based on the movieName
     const apiResult = await axios.get(API_URL, {
       params: {
         apikey: yourAPIKey,
-        t: movieName
+        s: movieName
       },
     });
-    const movie = apiResult.data;
-    const result = await db.query("SELECT * FROM movies WHERE imdbID = $1", [movie.imdbID]);
-    if (result.rows.length > 0) {
-      res.render("movie-details.ejs", { movie: result.rows[0] });
-    }
-    else {
-      res.render("add-movie.ejs", {
-        data: {
-          Title: movie.Title,
-          Year: movie.Year,
-          Genre: movie.Genre,
-          Writer: movie.Writer,
-          Plot: movie.Plot,
-          Poster: movie.Poster,
-          imdbID: movie.imdbID
-        }
+
+    const searchData = apiResult.data;
+    const movies = searchData.Search; // Array of movies returned
+
+    if (movies && movies.length > 0) {
+      // Render search results for user to select a movie
+      res.render("movie-list.ejs", { movies: movies });
+    } else {
+      // Handle case where no movies were found
+      res.render("index.ejs", {
+        movies: [],
+        error: 'No movies found. Please try again with a different search term.'
       });
     }
+
   } catch (err) {
     console.error(err);
     res.render("index.ejs", {
       movies: [],
       error: 'Error fetching data from the API. Please try again later.'
+    });
+  }
+});
+
+// Assume you have a route to handle movie selection from the list
+app.get("/select/:imdbID", async (req, res) => {
+  const imdbID = req.params.imdbID;
+
+  try {
+    // Step 2: Fetch detailed information for the selected movie using imdbID
+    const apiResult = await axios.get(API_URL, {
+      params: {
+        apikey: yourAPIKey,
+        i: imdbID // Use 'i' parameter to get movie details by IMDb ID
+      },
+    });
+
+    const movieDetails = apiResult.data;
+
+    // Check if the movie already exists in your database
+    const dbResult = await db.query("SELECT * FROM movies WHERE imdbID = $1", [imdbID]);
+
+    if (dbResult.rows.length > 0) {
+      // Movie exists in database, render details
+      res.render("movie-details.ejs", { movie: dbResult.rows[0] });
+    } else {
+      // Movie doesn't exist in database, render add-movie form
+      res.render("add-movie.ejs", {
+        data: {
+          Title: movieDetails.Title,
+          Year: movieDetails.Year,
+          Genre: movieDetails.Genre,
+          Writer: movieDetails.Writer,
+          Plot: movieDetails.Plot,
+          Poster: movieDetails.Poster,
+          imdbID: movieDetails.imdbID
+        }
+      });
+    }
+
+  } catch (err) {
+    console.error(err);
+    res.render("index.ejs", {
+      movies: [],
+      error: 'Error fetching movie details from the API. Please try again later.'
     });
   }
 });
@@ -137,9 +168,9 @@ app.get("/search", async (req, res) => {
 app.post("/add", async (req, res) => {
   const movie = req.body.newMovie;
   try {
-    await db.query(
-      "INSERT INTO movies (title, year, genre, writer, plot, poster, rating, imdbID) " +
-      "VALUES ($1, $2, $3, $4, $5, $6, $7, $8)",
+    const result = await db.query(`
+      INSERT INTO movies (title, year, genre, writer, plot, poster, rating, imdbID)
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *;`,
       [
         movie.title,
         movie.year,
@@ -149,9 +180,10 @@ app.post("/add", async (req, res) => {
         movie.poster,
         movie.rating,
         movie.imdbID
-      ]
-    );
-    res.render("movie-details.ejs", { movie: movie });
+      ]);
+
+    res.render("movie-details.ejs", { movie: result.rows[0] });
+
   } catch (err) {
     console.error(err);
     res.render("index.ejs", {
